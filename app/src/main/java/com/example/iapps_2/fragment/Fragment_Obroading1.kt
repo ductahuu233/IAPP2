7c633c43d8f07f6dcd3686c1f722e9a167391e47
ducth <ducth@techasians.com>
2023-09-20 13:46:43 +0700

    Duc push

---
+++ app/src/main/java/com/example/iapps_2/fragment/Fragment_Obroading1.kt
@@ -0,0 +1,20 @@
+package com.example.iapps_2.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.iapps_2.R
+
+class Fragment_Obroading1 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_onbroading1, container, false)
+    }
+
+
+}
